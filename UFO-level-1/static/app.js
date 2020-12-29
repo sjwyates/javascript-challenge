28665d36014bdfdc09897e35209e5b2a50240269
Stuart Yates <sjwyates@Mac-mini.local>
2020-12-29 15:22:05 -0700

    upper and title casing, cleaned up code

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -1,3 +1,4 @@
+// use fetch API to get data from json file, then populate table
 fetch('../../data.json')
     .then(res => res.json())
     .then(data => populateTable(data))
@@ -6,14 +7,31 @@ fetch('../../data.json')
 
 function populateTable(data) {
     const tbody = document.getElementById('sightings');
-    data.forEach(sighting => tbody.append(makeRow(sighting)));
+    const trows = data.map(makeRow);
+    trows.forEach(trow => tbody.append(trow));
 }
 
-function makeRow(sighting) {
+function makeRow(sighting, index) {
     const tr = document.createElement('tr');
+    // index the rows
+    const th = document.createElement('th');
+    th.innerHTML = index + 1;
+    tr.append(th);
+    // create each table cell
     ['datetime', 'city', 'state', 'country', 'shape', 'durationMinutes', 'comments'].forEach(column => {
         const td = document.createElement('td');
-        td.innerHTML = sighting[column];
+        let text = sighting[column];
+        // clean up the casing
+        if (column == 'state' || column == 'country') {
+            text = text.toUpperCase();
+        }
+        if (column == 'city' || column == 'shape') {
+            text = text.split(' ')
+                .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                .join(' ');
+        }
+        // add the text and append the cell
+        td.innerHTML = text;
         tr.append(td);
     })
     return tr;
