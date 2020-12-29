f08729468030a87344c5bd3cec52300c8e8fbf0f
Stuart Yates <sjwyates@Mac-mini.local>
2020-12-29 15:04:29 -0700

    populated table

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -1,9 +1,20 @@
 fetch('../../data.json')
     .then(res => res.json())
     .then(data => populateTable(data))
-    .catch(err => console.error(err))
+    .catch(err => console.error(err));
 
 
 function populateTable(data) {
-    console.log(data[0])
+    const tbody = document.getElementById('sightings');
+    data.forEach(sighting => tbody.append(makeRow(sighting)));
+}
+
+function makeRow(sighting) {
+    const tr = document.createElement('tr');
+    ['datetime', 'city', 'state', 'country', 'shape', 'durationMinutes', 'comments'].forEach(column => {
+        const td = document.createElement('td');
+        td.innerHTML = sighting[column];
+        tr.append(td);
+    })
+    return tr;
 }
