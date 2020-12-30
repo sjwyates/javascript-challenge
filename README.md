6fbeb1f5998fe2f9e99ee17cc36f08d2e05b7ad8
Stuart Yates <syates@cmcbio.com>
2020-12-30 12:14:14 -0700

    readme updates, minor bug fix to makeTable, and refactored applyFilters

--- README.md
+++ README.md
@@ -2,20 +2,73 @@
 
 ## Overview
 
-In this assignment, you're given some dummy data about UFO sightings from 2010. The first goal is to use JavaScript to populate an HTML table from that data, and the second goal is to create form fields and use the inputs to filter the data and rebuild the table.
+In this assignment, you're given some dummy data about UFO sightings from 2010. The assignment has 2 parts: first, use JavaScript to populate an HTML table from that data; and second, create a form with some filter options (date, city, UFO shape, etc) and implement event listeners/handlers to selectively rebuild the table.
 
 ![ufo page screenshot](images/ufo-home.png)
 
-## JavaScript
-
-### Frameworks
-
 When it came to frameworks, I decided to go with [Vanilla JS](http://vanilla-js.com/), because it's simple, fast, and comes pre-loaded on all web browsers. It can handle DOM traversal and manipulation, plus AJAX, which is all I really needed.
 
-### Fetch API
+## Part 1 - Creating the Table
 
-Instead of loading the dummy data from a JS file, I converted it into JSON to make it a little more realistic, then used the Fetch API to retrieve it, used the json() method to extract the data, then cleaned it up a little by doing title and upper-casing where appropriate. I then chained a few more then() methods to call the function that builds the table, plus another function to populate the filter category dropdowns.
+### Fetching and cleaning the data
 
-### Forms
+Instead of loading the dummy data from a JS file, I converted it into JSON to make it a little more realistic, then used the Fetch API to retrieve it. Once the promise is fulfilled, it gets passed through a pipeline of `then()` methods.
 
-Lastly, I built out a form that allows the user to filter by date range, city, state, country, and shape. There's an apply button of type="submit" and a clear button of type="reset". I then added event listeners to the form and gave them handlers. Each destroys the table, does logic to determine what data to put in the rebuilt table, and calls the original makeTable() function. And they have the preventDefault() method so that only the table refreshes, not the whole page.
+First, `res => res.json()` extracts the JSON data and returns it as a POJO, then `cleanData` returns a mapped array with upper-cased the state and country codes and title-cased cities and shapes. (It also assigns the array to the global `jsonData` variable so it can be accessed later by the `applyFilters` and `clearFilters` functions.)
+
+### Creating the table
+
+The next callback in the promise pipeline is `makeTable`, which runs a `forEach` loop that calls `makeRow` on each UFO sighting. That function creates a `<tr>` element, then runs a `forEach` loop on an array of all the column keys:
+
+```
+["datetime", "city", "state", "country", "shape", durationMinutes", "comments"].forEach()
+```
+
+Each iteration creates a `<td>` element, sets its `innerHTML` to the value in the sighting object, and appends it to the `<tr>`. Once it's looped through all the columns, it returns the `<tr>`.
+
+Finally, `makeTable` appends each returned `<tr>` to the `<tbody>` element, then returns the data so it can be passed to the final callback in the pipeline.
+
+## Part 2 - Listen, Handle, Filter, Rebuild
+
+### Populating the dropdown menus
+
+Picking up where we left off in part 1, the last callback is `makeDropdowns`, which populates the dropdowns in the filter form for the 4 categories (city, state, country, and shape).
+
+First, it needs to find all the unique values in the dataset. It runs a `forEach` loop on an array of the 4 categories:
+
+```
+["city", "state", "country", "shape"].forEach()
+```
+
+Each iteration maps the dataset to get an array of all values for that category. To get unique values, it passes the array into the `Set` constructor, then uses the spread operator to convert the set back to an array:
+
+```
+[...new Set(data.map((sighting) => sighting[category]))]
+```
+
+It then passes each array of unique values into the `makeDropdown` function, along with the id of their target `<select>` element.
+
+That function gets the `<select>` element by ID, then runs a `forEach` loop that creates an `<option>` element for each choice, sets its `value` and `innerHTML`, and appends it to the `<select>` element.
+
+### The filter form
+
+In addition to those 4 category dropdowns, the filter form contains 2 `<input>` elements of `type="date"` so the user can choose a start and end date. I didn't implement anything custom for this, I just left it up to the browser's native date picker.
+
+At the bottom of the form, there are 2 buttons: the Apply button with `type="submit"`, and the Clear button with `type="reset"`.
+
+### Event listening and handling
+
+There are 2 event listeners, both applied to the form:
+
+- `submit` triggers the `applyFilters` handler
+- `reset` triggers the `clearFilters` handler
+
+Both handlers take in the event and call `preventDefault()` on it to stop the event from bubbling up to the top level of the DOM, since that would trigger an unwanted page refresh.
+
+### Applying the filters
+
+The `applyFilters` handler isn't super elegant, but it works. First, it creates a shallow copy of the global `jsonData` array using a spread operator and assigns it to a variable called `filteredData`. I used `let` to declare it so I could keep reassigning it to a filtered version of itself.
+
+Next, it loops over the 4 dropdown categories and checks to see if its `<select>` element has a value, and if it does, it filters `filteredData` to find matches, then reassigns it to the filtered array and moves on to the next category.
+
+For the date range, it takes the value from each date input
--- UFO-level-2/static/app.js
+++ UFO-level-2/static/app.js
@@ -3,134 +3,151 @@
 // -------------------------------------
 let jsonData = [];
 
-fetch('../../data.json')
-    .then(res => res.json())
-    .then(cleanData)
-    .then(makeTable)
-    .then(makeDropdowns)
-    .catch(err => console.error(err));
-    
+fetch("../../data.json")
+  .then((res) => res.json())
+  .then(cleanData)
+  .then(makeTable)
+  .then(makeDropdowns)
+  .catch((err) => console.error(err));
 
 function cleanData(data) {
-    function toTitleCase(str) {
-        return str.split(' ')
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-        .join(' ');
-    }
-    jsonData = data.map(sighting => {
-        let { datetime, city, state, country, shape, durationMinutes, comments } = sighting;
-        return {
-            datetime,
-            city: toTitleCase(city),
-            state: state.toUpperCase(),
-            country: country.toUpperCase(),
-            shape: toTitleCase(shape),
-            durationMinutes,
-            comments
-        }
-    });
-    return jsonData;
+  function toTitleCase(str) {
+    return str
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  }
+  jsonData = data.map((sighting) => {
+    let {
+      datetime,
+      city,
+      state,
+      country,
+      shape,
+      durationMinutes,
+      comments,
+    } = sighting;
+    return {
+      datetime,
+      city: toTitleCase(city),
+      state: state.toUpperCase(),
+      country: country.toUpperCase(),
+      shape: toTitleCase(shape),
+      durationMinutes,
+      comments,
+    };
+  });
+  return jsonData;
 }
 
 // -------------------------------------
 // MAKE TABLE
 // -------------------------------------
 function makeTable(data) {
-    const tbody = document.getElementById('tbody');
-    const trows = data.map(makeRow);
-    trows.forEach(trow => tbody.append(trow));
-    document.querySelectorAll('th').forEach(th => th.style.color = 'white');
-    return data;
+  const tbody = document.getElementById("tbody");
+  const trows = data.map(makeRow);
+  trows.forEach((trow) => tbody.append(trow));
+  document.querySelectorAll("th").forEach((th) => (th.style.color = "white"));
+  return data;
 }
 
 function makeRow(sighting, index) {
-    const tr = document.createElement('tr');
-    // striping
-    if (index % 2 == 0) {
-        tr.classList.add('has-background-grey')
+  const tr = document.createElement("tr");
+  // striping
+  if (index % 2 == 0) {
+    tr.classList.add("has-background-grey");
+  }
+  // index the rows
+  const th = document.createElement("th");
+  th.innerHTML = index + 1;
+  tr.append(th);
+  // create each table cell
+  [
+    "datetime",
+    "city",
+    "state",
+    "country",
+    "shape",
+    "durationMinutes",
+    "comments",
+  ].forEach((column) => {
+    const td = document.createElement("td");
+    // set max width for comments column
+    if (column == "comments") {
+      td.style.maxWidth = "300px";
     }
-    // index the rows
-    const th = document.createElement('th');
-    th.innerHTML = index + 1;
-    tr.append(th);
-    // create each table cell
-    ['datetime', 'city', 'state', 'country', 'shape', 'durationMinutes', 'comments'].forEach(column => {
-        const td = document.createElement('td');
-        let text = sighting[column];
-        // set max width for comments column
-        if (column = 'comments') {
-            td.style.maxWidth = '300px';
-        }
-        // add the text and append the cell
-        td.innerHTML = text;
-        tr.append(td);
-    })
-    return tr;
+    // add the text and append the cell
+    td.innerHTML = sighting[column];
+    tr.append(td);
+  });
+  return tr;
 }
 
 // -------------------------------------
 // DROPDOWNS
 // -------------------------------------
 function makeDropdowns(data) {
-    // get all unique values
-    ['city', 'state', 'country', 'shape'].forEach(category => {
-        const id = category + 'Select';
-        const choices = [...new Set(data.map(sighting => sighting[category]))]
-        makeDropdown(id, choices);
-    })
-    return data;
+  // get all unique values
+  ["city", "state", "country", "shape"].forEach((category) => {
+    const id = category + "Select";
+    const choices = [...new Set(data.map((sighting) => sighting[category]))];
+    makeDropdown(id, choices);
+  });
+  return data;
 }
 
 function makeDropdown(id, choices) {
-    const select = document.getElementById(id);
-    choices.forEach(choice => {
-        const option = document.createElement('option');
-        option.value = choice;
-        option.innerHTML = choice;
-        select.append(option)
-    })
+  const select = document.getElementById(id);
+  choices.forEach((choice) => {
+    const option = document.createElement("option");
+    option.value = choice;
+    option.innerHTML = choice;
+    select.append(option);
+  });
 }
 
 // -------------------------------------
 // EVENT HANDLING
 // -------------------------------------
-const filterForm = document.getElementById('filterForm')
-filterForm.addEventListener('submit', applyFilters);
-filterForm.addEventListener('reset', clearFilters);
-
+const filterForm = document.getElementById("filterForm");
+filterForm.addEventListener("submit", applyFilters);
+filterForm.addEventListener("reset", clearFilters);
 
 function applyFilters(e) {
-    e.preventDefault();
-    let filteredData = [...jsonData];
-    ['city', 'state', 'country', 'shape'].forEach(category => {
-        const value = document.getElementById(category + 'Select').value;
-        if (value) {
-            filteredData = filteredData.filter(sighting => sighting[category] == value);
-        }
-    })
-    const startDate = document.getElementById('startDate').value;
-    if (startDate) {
-        filteredData = filteredData.filter(sighting => new Date(startDate) <= new Date(sighting.datetime))
+  e.preventDefault();
+  let filteredData = [...jsonData];
+  ["city", "state", "country", "shape"].forEach((category) => {
+    const value = document.getElementById(category + "Select").value;
+    if (value) {
+      filteredData = filteredData.filter(
+        (sighting) => sighting[category] == value
+      );
     }
-    const endDate = document.getElementById('endDate').value;
-    if (endDate) {
-        filteredData = filteredData.filter(sighting => new Date(endDate) >= new Date(sighting.datetime))
-    }
-    rebuildTable(filteredData);
+  });
+  filteredData = filteredData.filter(
+    (sighting) =>
+      new Date(document.getElementById("startDate").value) <=
+        new Date(sighting.datetime) &&
+      new Date(document.getElementById("endDate").value) >=
+        new Date(sighting.datetime)
+  );
+  rebuildTable(filteredData);
 }
 
 function clearFilters(e) {
-    e.preventDefault();
-    document.getElementById('startDate').value = '2010-01-01';
-    document.getElementById('endDate').value = '2010-01-30';
-    ['citySelect', 'stateSelect', 'countrySelect', 'shapeSelect'].forEach(id => document.getElementById(id).value = '')
-    rebuildTable(jsonData);
+  e.preventDefault();
+  document.getElementById("startDate").value = "2010-01-01";
+  document.getElementById("endDate").value = "2010-01-30";
+  ["citySelect", "stateSelect", "countrySelect", "shapeSelect"].forEach(
+    (id) => (document.getElementById(id).value = "")
+  );
+  rebuildTable(jsonData);
 }
 
 function rebuildTable(data) {
-    const tbody = document.getElementById('tbody');
-    while (tbody.firstChild) {
-        tbody.removeChild(tbody.firstChild);
-    }
-    makeTable(data);
+  const tbody = document.getElementById("tbody");
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+  makeTable(data);
 }
